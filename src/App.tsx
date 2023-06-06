5dba10cafa87b86e4b25dbcadcebb84c546708f5
krocksa <rogeryonatan@gmail.com>
2023-06-06 00:06:57 -0400

    Segundo Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Component } from './Components/Component';
 
 function App() {
   return (
     <>
-      
+      <Component/>
     </>
   );
 }
---
+++ src/Components/Component.tsx
@@ -0,0 +1,14 @@
+interface ComponentProps{
+
+}
+
+export const Component: React.FC<ComponentProps> = ({}) => {
+
+    return(
+        <>
+            <h1>
+                Hola Roger
+            </h1>
+        </>
+    )
+}
